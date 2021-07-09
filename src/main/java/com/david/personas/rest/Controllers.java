6bc984a3a2f295fae11049e6dc7ea01ad40676e2
David Viuche <jviucheroja@uniminuto.edu.co>
2021-07-08 21:30:11 -0500

    creacion de servicio para buscar una persona por el id

--- src/main/java/com/david/personas/rest/Controllers.java
+++ src/main/java/com/david/personas/rest/Controllers.java
@@ -1,10 +1,12 @@
 package com.david.personas.rest;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +22,18 @@ public class Controllers {
 	private PersonasDAO personasDAO;
 	
 	@GetMapping
-	public ResponseEntity<List<Persona>> getProduct(){
+	public ResponseEntity<List<Persona>> getPersonas(){
 		List<Persona> personas = personasDAO.findAll();
 		return ResponseEntity.ok(personas);
 	}
 	
+	@RequestMapping(value= "{id_persona}")
+	public ResponseEntity<Persona> getPersonaById(@PathVariable Long id_persona){
+		Optional<Persona> optionalPersona = personasDAO.findById(id_persona);
+		if(optionalPersona.isPresent()) {
+			return ResponseEntity.ok(optionalPersona.get());
+		}else {			
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
